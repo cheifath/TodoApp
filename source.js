2605828ec40bb262d8e0420b0229e2afb5ae8d21
Cheifath <cheifathmohammed@gmail.com>
2024-12-13 22:06:20 +0530

    Add final product

---
+++ source.js
@@ -0,0 +1,45 @@
+checkBoxes=document.querySelectorAll(".checkboxes");
+texts=document.querySelectorAll(".texts");
+
+body=document.querySelector("body");
+searchBar=document.querySelector(".search-bar");
+allSelect=document.querySelector(".all-select");
+icon=document.querySelector(".icon-2");
+let isLight=true;
+
+
+
+icon.addEventListener('click',()=>{
+    if(isLight==true){
+        body.style.backgroundColor="#313D5A";
+        body.style.color="#FFF2F1";
+        searchBar.style.backgroundColor="#313D5A";
+        allSelect.style.color="#313D5A";
+        icon.style.color="#313D5A";
+        texts.forEach(text => {
+            text.style.color="#66717E";
+        });
+        isLight=false;
+    }else if(isLight==false){
+        body.style.backgroundColor="#FFF2F1";
+        searchBar.style.backgroundColor="#FFF2F1";
+        allSelect.style.color="#FFF2F1";
+        icon.style.color="#FFF2F1";
+        isLight=true;
+    }
+    
+})
+
+const label=document.querySelectorAll("label");
+console.log(label);
+
+checkBoxes.forEach(checkbox => {
+    checkbox.addEventListener('change', ()=> { 
+        const label = document.querySelector(`label[for="${checkbox.id}"]`);
+        if (checkbox.checked) {
+            label.style.textDecoration = 'line-through';
+        } else {
+            label.style.textDecoration='none'; 
+        }
+    });
+});
