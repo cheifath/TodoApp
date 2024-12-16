ebdba43225faeab70f765c6bb2b33bb32caad0dc
Cheifath <cheifathmohammed@gmail.com>
2024-12-16 21:50:24 +0530

    Add feature

--- source.js
+++ source.js
@@ -28,16 +28,55 @@ icon.addEventListener('click',()=>{
         isLight=true;
     }
     
-})
+});
 
-// //line-through
-// checkBoxes.forEach(checkbox => {
-//     checkbox.addEventListener('change', ()=> { 
-//         const label = document.querySelector(`label[for="${checkbox.id}"]`);
-//         if (checkbox.checked) {
-//             label.style.textDecoration = 'line-through';
-//         } else {
-//             label.style.textDecoration='none'; 
-//         }
-//     });
-// });
+const bar = document.querySelector(".search-bar");
+const item = document.querySelector(".items");
+
+function addTask() {
+    if(bar.value===""){
+        alert("Please enter the text");
+    } else{
+        // item.value=bar.value;
+        let li = document.createElement("li"); //creating a new element dynamically
+        li.innerHTML = bar.value; //assigning the value of the input tag to the new element
+        item.appendChild(li); //adding the new element to a particular location
+
+        let span = document.createElement("span");
+        span.innerHTML= "&#10006";
+        li.appendChild(span);
+    }
+    saveTask();
+    //by calling this function the data is stored inside local os
+    bar.value=""; // setting the  input box value to null after the entry
+}
+
+function saveTask(){ //saving the entered text into local storage
+    localStorage.setItem("data",item.innerHTML);
+    //localStorage is the keyword associated with storing on local os
+    //setItem is also a keyword used to store or it put the data to local os
+    //"data" is the keyword like an id which is unique
+    //item.innerHTML is the element which is to be stored
+}
+
+function showtask(){ //show the saved data even if we refresh the website
+    item.innerHTML = localStorage.getItem("data");
+    //getItem is a keyword used to fetch the data we stored earlier without this the data is only stored cannot be seen
+}
+showtask();
+//by callling the this function which is defned to fetch the data stored in local system it has to be called outside of the functions
+
+item.addEventListener("click", function(e) { //adding event listener to the lists on click then creating a function with object
+
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        //target is used to access the specified elements
+        //classList is a class which is styled in css
+        //toggle is used to toggle on and off the style sheet in this case
+        saveTask();
+        //saving the changes made
+    } else if(e.target.tagName==="SPAN"){
+        e.target.classList.remove();
+    }
+
+})
